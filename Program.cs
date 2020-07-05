b6c71f16794f6fc3dded1a07dcc95919b3dd6f55
senaszel <senaszel@gmail.com>
2020-07-05 03:27:12 +0200

    31st ex

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Console;
+
+namespace SD_31_Watch_n_Write
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbOfSeries;
+
+            do
+            {
+                Clear();
+                WriteLine("How many times you wish to read n write series of numbers. Of which first is just a \"1\"?");
+                int.TryParse(ReadLine(), out int _out);
+                nbOfSeries = _out;
+
+            } while (nbOfSeries <= 0);
+
+            Clear();
+            Stack<int> series = new Stack<int>();
+            series.Push(1);
+
+            for (int i = 0; i < nbOfSeries; i++)
+            {
+                WriteLine(string.Concat("\tserie:", (i + 1)));
+                Stack<int> _temp = new Stack<int>(series);
+                series = Read(_temp);
+                Write(series);
+            }
+
+            Console.WriteLine("\n\t\ttheEnd ===============================");
+        }
+
+
+        private static void Write(Stack<int> series)
+        {
+            series.Reverse().ToList().ForEach(x => Console.Write(x)); WriteLine();
+        }
+
+
+        private static Stack<int> Read(Stack<int> series)
+        {
+            Stack<int> ret = new Stack<int>();
+            int counter = 1;
+            while (series.Count != 1)
+            {
+                var current = series.Pop();
+                if (current == (series.Peek()))
+                {
+                    counter++;
+                }
+                else
+                {
+                    ret.Push(counter);
+                    counter = 1;
+                    ret.Push(current);
+                }
+            }
+            ret.Push(counter);
+            ret.Push(series.Pop());
+
+            return ret;
+        }
+
+
+    } // end of class
+} // end of namespace
---
+++ README.md
@@ -0,0 +1,26 @@
+# 	SD_31_Watch_n_Write
+# 31st exercise for facebook group Nauczsie.NETa
+
+## About
+```
+The goal was to write a program that starting with string of "1".
+Reads the numbers and print how many of them it sees from left to right.
+The only variable is number of series it reads and writes. There's example below.
+```
+## Usage
+```
+To use program just pass the number of series 
+and program will look at the numbers and write how many of them it sees
+```
+
+## Example
+```
+if asks to read n write 4 times in a row it will print:
+1
+11 (program reads "one 1s")
+21 (it reads "two 1s")
+1211 (it reads "one 2s, one 1s")
+111221 (it reads "one 1s, one 2s, two 1s")
+```
+
+
